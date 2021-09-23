9fcbb80a280fee1b607bb64d4aecb001038db93a
TonofClay93 <88801144+TonofClay93@users.noreply.github.com>
2021-09-23 00:47:43 -0400

    initial code for the night

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+function getLocalStorage(key) {
+    let value = localStorage.getItem(key);
+    if (value) {
+        $(`#text${key}`).text(value);
+    }
+}
+
+$( document ).ready(function() {
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    for (let i = 9; i < 18; i++) {
+    
+        // row
+        var row = $(`<div data-time=${i} id='${i}' class="row">`);
+
+        // column
+        var col1 = $('<div class="col-sm-2"> <p class="hour">' + formatAMPM(i) + '</p>');
+
+        // column 2
+        var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`);        
+
+        // column 3
+        var col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
+
+        // col to row
+        row.append(col1);
+        row.append(col2);
+        row.append(col3);
+
+        // adding rows to container
+        $(".container").append(row);
+
+        getLocalStorage(i);
+    }});
