1e79f80face1365c397aa59a0ac87a1e66c38a48
TonofClay93 <88801144+TonofClay93@users.noreply.github.com>
2021-09-24 23:04:26 -0400

    final js code after error testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,17 +17,49 @@ $( document ).ready(function() {
 
         // column 2
         var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`);        
-
+        
         // column 3
         var col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
-
+        
         // col to row
         row.append(col1);
         row.append(col2);
         row.append(col3);
 
-        // adding rows to container
+        // add rows to container
         $(".container").append(row);
 
         getLocalStorage(i);
-    }});
+    }
+        
+    function formatAMPM(hours) {
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        return hours + ampm;
+    }
+formatAMPM();
+
+function updateColors(){
+        var currentTime = new Date().getHours();
+        for (var i = 9; i < 18; i++) { 
+        console.log(currentTime, $(`#${i}`).data("time"));
+            if ($(`#${i}`).data("time") == currentTime){
+            $(`#text${i}`).addClass( "present");
+        }   else if (currentTime < $(`#${i}`).data("time")) {
+            $(`#text${i}`).addClass( "future");
+        }
+    }
+}
+
+setInterval(function() {
+    updateColors();
+}, 1000);
+
+var saveBtn = $('.saveBtn');
+    saveBtn.on('click', function(){
+        let eventId = $(this).attr('id');
+        let eventText = $(this).parent().siblings().children('.description').val();
+        localStorage.setItem(eventId, eventText);
+    });
+});
